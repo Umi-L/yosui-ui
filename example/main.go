ff5bcfb5c1311d7c7fde54e50035ebc9951392d6
Julian O'Grady <julianogrady07@gmail.com>
2023-01-27 23:10:26 -0500

    working example

--- example/main.go
+++ example/main.go
@@ -1,8 +1,8 @@
-package yosui
+package main
 
 import (
+	"fmt"
 	"log"
-	"testing"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
@@ -25,11 +25,21 @@ type Game struct {
 }
 
 func (g *Game) Update() error {
+
+	if g.Gui.MainMenu.PlayButton.IsPressed() {
+		fmt.Print("Button Pressed \n")
+	}
+
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 
+	w, h := screen.Size()
+
+	g.Gui.Root.SetTransform(gui.Transform{X: 0, Y: 0, W: float32(w), H: float32(h)})
+
+	g.Gui.Root.DrawTree(screen)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
@@ -42,7 +52,7 @@ type GameUI struct {
 }
 
 type MainMenuUI struct {
-	PlayButton GuiButton
+	PlayButton *GuiButton
 }
 
 type GuiButton struct {
@@ -66,7 +76,7 @@ func NewButton(image *ebiten.Image) GuiButton {
 	}
 }
 
-func TestUi(t *testing.T) {
+func main() {
 
 	game := Game{}
 
@@ -90,6 +100,8 @@ func TestUi(t *testing.T) {
 	mainMenu.AddChild(&playButton)
 
 	playButton.SetTransform(trans)
+
+	game.Gui.MainMenu.PlayButton = &playButton
 
 	//add to gui
 	game.Gui.Root.AddChild(&mainMenu)
